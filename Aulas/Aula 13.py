54da600943858a343864d48d90b27d605ddf2439
magalhaesg <magalhaesg605@gmail.com>
2023-07-20 18:28:06 -0300

    Atualização de exercicios

--- Aulas/Aula 13.py
+++ Aulas/Aula 13.py
@@ -1,4 +1,23 @@
 n=0
-for c in range(1,10):
+for c in range(0,6):
     n = n + 1
     print(f'Teste {n}')
+
+print('____________________________________')
+for d in range (6, 0, -1):
+    print(d)
+
+print('____________________________________')
+
+n = int(input('Digite um número: '))
+for e in range(1,n):
+    print(e)
+
+print('____________________________________')
+
+for t in range(0, 6, 2):
+    print(t)
+
+print('____________________________________')
+
+
